17acaff2bd300e354ea4f0e471c21030892d66ca
anwar <anwaralam@technerdslab.com>
2021-01-04 19:46:15 +0500

    created Model

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiresource('/products', ProductController::class);
+
+Route::group(['prefix '=>'products'] ,function(){
+    Route::apiResource('/{product}/reviews',ReviewController::class);
+});
+//Route::Resource('/products',[ProductController::class]);
